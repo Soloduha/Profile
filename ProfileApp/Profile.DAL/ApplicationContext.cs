c46716c0cef3ccd01739bcc3d941054abd2b88e0
Soloduha Vlad <soloduha_vlad@ukr.net>
2018-11-08 17:51:34 +0200

    1).DAL,BLL libraries was added. 2)Identity was configured. 3)Register, Login, SuccessRegister models was added. 4)Basic logic for adding and loggining was added in BLL.

--- ProfileApp/Profile.DAL/ApplicationContext.cs
+++ ProfileApp/Profile.DAL/ApplicationContext.cs
@@ -18,9 +18,5 @@ namespace Profile.DAL
         public DbSet<ClientProfile> ClientProfiles { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
+
 }
---
+++ ProfileApp/ProfileApp/App_Start/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Profile.BLL.Interfaces;
+using Profile.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+[assembly: OwinStartup(typeof(ProfileApp.App_Start.Startup))]
+
+namespace ProfileApp.App_Start
+{    
+    public class Startup
+    {
+        IServiceCreator serviceCreator = new ServiceCreator();
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IUserService>(CreateUserService);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        private IUserService CreateUserService()
+        {
+            return serviceCreator.CreateUserService("DefaultConnection");
+        }
+    }
+}
